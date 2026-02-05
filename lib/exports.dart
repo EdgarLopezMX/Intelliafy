bc1454872143d57b04b94c9448fd36fd6b8efb94
Edgar Ismael López Rosas <Edgar.LopezRosas@hotmail.com>
2026-02-04 21:06:44 -0600

    refactor: Created a global exports.dart file.

---
+++ lib/exports.dart
@@ -0,0 +1,18 @@
+//Flutter
+export 'package:flutter/material.dart';
+export 'package:flutter/gestures.dart';
+
+//Firebase and external services
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_auth/firebase_auth.dart';
+export 'package:cloud_firestore/cloud_firestore.dart';
+
+//Providers and utilities
+export 'package:provider/provider.dart';
+export 'package:fluttertoast/fluttertoast.dart';
+
+//Intellify
+export 'package:intelliafy_app/config/app_theme.dart';
+export 'package:intelliafy_app/providers/auth_notifier.dart';
+export 'package:intelliafy_app/widgets/widgets.dart';
+export 'package:intelliafy_app/screens/screens.dart';
